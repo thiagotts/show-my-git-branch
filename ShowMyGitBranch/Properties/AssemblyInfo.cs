706760d9a3f8ec74864d21e3ca7f14e3267d4449
Thiago Sá <thiagottsa@gmail.com>
2015-07-28 19:55:09 -0300

    Updated assembly version number. Removed unused test file.

--- ShowMyGitBranch/Properties/AssemblyInfo.cs
+++ ShowMyGitBranch/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 
 [assembly: AssemblyTitle("ShowMyGitBranch")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Displays the name of your current git branch in Visual Studio's title bar.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Thiago Sa")]
 [assembly: AssemblyProduct("ShowMyGitBranch")]
@@ -30,7 +30,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
 [assembly: InternalsVisibleTo("ShowMyGitBranch_IntegrationTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f9e7ae70ea230acb7f3f69c784ee86d32603a94e6017a6c461ea166cad5ec810706de5a2840dfd3b95de2d72465081d0b563127fa0f65ff0dd6b049e7dbeea3baa6eca911b86ca724eb015625ea8095a2415cf9b3a6ce2b249f98aaf096bd0f041560bcafdbab98b27663b64d2acd5190b54d5aa0cce20bb3134e7a2e46f26c0")]
 [assembly: InternalsVisibleTo("ShowMyGitBranch_UnitTests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100f9e7ae70ea230acb7f3f69c784ee86d32603a94e6017a6c461ea166cad5ec810706de5a2840dfd3b95de2d72465081d0b563127fa0f65ff0dd6b049e7dbeea3baa6eca911b86ca724eb015625ea8095a2415cf9b3a6ce2b249f98aaf096bd0f041560bcafdbab98b27663b64d2acd5190b54d5aa0cce20bb3134e7a2e46f26c0")]
